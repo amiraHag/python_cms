eb4f90126662aa1601e5f95a4bbd5d4b795e399b
Amira <amira071846@feng.bu.edu.eg>
2021-04-02 03:23:42 +0200

    template inheretance

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', page_title='Home Page')
+
+
+@app.route('/shop')
+def shop_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('shop.html', shop_items=items, page_title='Shop Page')
 
 
 @app.route('/about')
