3189c8dce844fe7db8e90dc6dd2d8bcce7cbbfa7
Amira <amira071846@feng.bu.edu.eg>
2021-05-17 05:28:06 +0200

    Flask Validations

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 
 @app.route('/')
@@ -21,9 +22,20 @@ def about_page():
     return 'About Page'
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(user_name=form.username.data,
+                              email_address=form.email.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('shop_page'))
+    if form.errors != {}:
+        for err_message in form.errors.values():
+            print(f' There was an error in creating a User {err_message} ')
+
     return render_template('register.html', form=form, page_title='Register Page')
 
 
